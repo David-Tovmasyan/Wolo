edb2335573ea23ba5f99de959867128c8bfa8d98
David-Tovmasyan <88345812+David-Tovmasyan@users.noreply.github.com>
2023-03-13 18:37:53 +0100

    Interface Update
    Added new types of interfaces. 
    Added description.
    Now interfaces show how application looks like.

--- interfaces/interface.ts
+++ interfaces/interface.ts
@@ -1,10 +1,24 @@
-export interface Iuser{
+// we have 2 types of user. First is just a user and second is company. 
+// When registrating people gonna choose what type of account they want to have (normal or company)
+
+//Both types are Iaccount
+export interface Iaccount{
+    id:number,
     login:string,
     password:string,
     username:string,
     email:string,
+    profilePicture?:string,
+    phone?:string
+    accountType:boolean
 }
 
+// Users can follow companies 
+export interface Iuser extends Iaccount{
+    following:Array<Icompany>,
+}
+
+// Icompany is the same Iaccount + company parameters. Company parameters are posting, company's description and Location 
 export default interface Icompany extends Iuser{
     posts:Array<Ipost>,
     description:string,
@@ -12,5 +26,8 @@ export default interface Icompany extends Iuser{
 }
 
 export interface Ipost{
-    image:string
+    postId:number,
+    image:string,
+    publishDate:Date,
+    postDescription:string
 }
